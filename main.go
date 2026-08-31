e97689d24b6dd6fcaa0c4e5fa983163744edb1ee
Rafaela Pessanha <rafaelapessanhasouza@gmail.com>
2026-08-31 11:36:29 +0200

    Fix main.go static with mux

--- main.go
+++ main.go
@@ -16,33 +16,26 @@ const (
 )
 
 func main() {
+	// --- Actual directory
 	dir, err := os.Getwd()
 	if err != nil {
 		log.Fatal(err)
 	}
-	fs := http.FileServer(http.Dir("./static"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	// --- Templates config
 	templates := template.Must(template.ParseFiles(
 		filepath.Join(dir, "templates", "pages", "home.html"),
 		filepath.Join(dir, "templates", "base", "header.html"),
 		filepath.Join(dir, "templates", "base", "footer.html"),
 	))
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		data := struct {
-			Title string
-		}{
-			Title: "Home - MetaRock",
-		}
-		err := templates.ExecuteTemplate(w, "home", data)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			log.Println(err)
-		}
-	})
-	fmt.Println("Server running at http://localhost:" + serverPort())
-	connection := http.ListenAndServe(":8080", routes())
-	if connection != nil {
-		log.Fatalf("démarrage du serveur: %v", connection)
+	router := routes(templates)
+
+	// --- Start server
+	port := serverPort()
+	fmt.Printf("Server running at http://localhost:%s\n", port)
+
+	if err := http.ListenAndServe(":"+port, router); err != nil {
+		log.Fatalf("Erro ao iniciar o servidor: %v", err)
 	}
 }
 
@@ -53,23 +46,40 @@ func serverPort() string {
 	return defaultPort
 }
 
-func routes() http.Handler {
+func routes(templates *template.Template) http.Handler {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", homeHandler)
+
+	// --- Static route mux
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	mux.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	// --- Route principal white the templates
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/" {
+			http.NotFound(w, r)
+			return
+		}
+
+		data := struct {
+			Title string
+		}{
+			Title: "Home - MetaRock",
+		}
+
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		err := templates.ExecuteTemplate(w, "home", data)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			log.Println(err)
+		}
+	})
+
+	// --- Routes API
 	mux.HandleFunc("/api", api.ApiHandler)
 	mux.HandleFunc("/artists", api.DataHandler)
 	mux.HandleFunc("/locations", api.DataHandler)
 	mux.HandleFunc("/dates", api.DataHandler)
 	mux.HandleFunc("/relation", api.DataHandler)
 	return mux
-}
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
-	}
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprint(w, "<h1>Groupie Tracker</h1><p>Le serveur fonctionne.</p>")
 }
