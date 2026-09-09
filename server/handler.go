3628d25b344582f531f3a552720bfdff8c1bdbdf
Léandre Mauger-Longuemare <maugerlonguemareleandre@gmail.com>
2026-09-09 16:09:01 +0200

    fix: expose artist names to home search datalist

--- server/handler.go
+++ server/handler.go
@@ -25,13 +25,15 @@ func Routes(templates *template.Template, data *AppData, fullArtists []ArtistFul
 			return
 		}
 		homeData := struct {
-			Title   string
-			Artists []ArtistFull
-			Query   string
+			Title       string
+			Artists     []ArtistFull
+			Suggestions []ArtistFull
+			Query       string
 		}{
-			Title:   "Home - MetaRock",
-			Artists: []ArtistFull{}, // empty list
-			Query:   "",
+			Title:       "Home - MetaRock",
+			Artists:     []ArtistFull{}, // empty list
+			Suggestions: fullArtists,
+			Query:       "",
 		}
 		w.Header().Set("Content-Type", "text/html; charset=utf-8")
 		err := templates.ExecuteTemplate(w, "home", homeData)
